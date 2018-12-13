560bd43e0b74b25451cf2ef7a67aa1f2d3d9becc
William Fischer <william.fischer42@gmail.com>
2018-12-13 09:37:46 -0700

    Added todo

--- app/controllers/user/activation_controller.rb
+++ app/controllers/user/activation_controller.rb
@@ -1,5 +1,6 @@
 class User::ActivationController < ApplicationController
   def create
+    # TODO: Set this up to redirect to a "successful activation" page, then the dashboard. Make sure to test.
     if current_user && current_user.id == params[:id].to_i
       current_user.update(active: true)
     end
